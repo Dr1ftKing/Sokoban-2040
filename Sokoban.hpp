a7cd1ee2b7db76d9d038b611d11430b492de61a0
J <59805734+Dr1ftKing@users.noreply.github.com>
2022-11-01 18:15:53 -0400

    Add files via upload

--- Sokoban.hpp
+++ Sokoban.hpp
@@ -5,50 +5,72 @@
 #include <exception>
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <memory>
+#include <vector>
+#include <string>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
-#include <vector>
-#include <string>
 
+/*
+The Sokoban class should have the following features:
+
+It must be sf::Drawable, with a private virtual void method named draw (as required of sf::Drawable subclasses).
+
+A movePlayer() method that takes an enumeration corresponding to one of the car- dinal directions that changes the player's location.
+For this part, movePlayer() is allowed to do nothing. You will complete this for part (b).
+
+It may contain a sf::Sprite object (as well as the sf::Texture object needed to hold the Sprite's image).
+
+A >> operator which reads the level from a stream. You may want to add a << operator that writes the level back to a stream.
+*/
 
 class Sokoban : public sf::Drawable {
  public:
     // constructors
     Sokoban();
-    explicit Sokoban(std::string image_filename);
-    explicit Sokoban(const Sokoban& copySokoban);
-    explicit Sokoban(Sokoban&& moveSokoban) noexcept;
-    ~Sokoban();
-
-    // accessors and mutators
-    int getXPlane(void) const;
-    int getYPlane(void) const;
-    sf::Image getImage(void) const;
-
-
-    void setXPlane(int);
-    void setYPlane(int);
-    void setImage(std::string imageFileName);
-
-    // overloaded assignment operators for Sokoban setup in main
-    Sokoban& operator=(Sokoban& cpyInptVal);
-    Sokoban& operator=(Sokoban&& mvInptVal) noexcept;
 
 
     // overloaded insertion operator for file input through terminal
  private:
     // override virtual draw function
+    friend std::istream& operator>>(std::istream& in, Sokoban& Sokoban);
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-    int xPlane;
-    int yPlane;
-    sf::Image image; // image data for Sokoban
-    sf::Texture texture;
-    sf::Sprite sprite;
+
 };
-std::istream& operator>>(std::istream& in, Sokoban& Sokoban);
-std::ostream& operator<<(std::ostream& out, Sokoban& Sokoban);
+
+class SokobanMap : public sf::Drawable {
+ public:
+    // constructors
+    SokobanMap();
+    // accessors and mutators
+    int getColumn(void) const;
+    int getRow(void) const;
+    void setTextureMap(std::string imageFileName);
+
+ private:
+    // override virtual draw function
+    friend std::istream& operator>>(std::istream& in, SokobanMap& SokobanMap);
+    void drawMap(void);
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+    int columnNumber;
+    int rowNumber;
+    int wall;
+    int nonWall;
+    sf::Texture textureWall;
+    sf::Texture textureMap; // for non wall textures
+    // create helper class
+    // bare bones means nothing in it, maybe default construcotr.
+    // then create istream operator, that reads in data. From level file. Col number and row number. add these fields to sokoban
+    //  have draw function just draw, havent read data in level, just dimensions
+    // at this point should have correct dimensions
+    // determine how to represent map,(helper class?)
+    // concern should be whether its a wall or non wall
+    // then modify draw to do wall or floor
+
+    // should I store all 5 things together or wall.floor in one conatainer and the other three in another
+};
+
+// std::ostream& operator<<(std::ostream& out, Sokoban& Sokoban);
 
 #endif  // _HOME_OEM_DESKTOP_PROJECT_PROJECT_4_PS4A_SOKOBAN_HPP_
--- main.cpp
+++ main.cpp
@@ -1,38 +1,34 @@
-// Copyright Juan Lopez [UMASS_Lowell] [06/06/2022]
+// Copyright Juan Lopez [UMASS_Lowell] [10/2022]
 #include <cmath>
 #include "Sokoban.hpp"
 
 int main(int argc, char* argv[]) {
-        if (argc != 1) {
+    if (argc != 2) {
         std::cerr <<\
          "Incorrect usage. Usage:./Sokoban level1.txt"\
           << std::endl;
         exit(-1);
     }
-    int _inputXPlane;
-    int _inputYPlane;
-    std::cin >> _inputXPlane;
-    std::cin >> _inputYPlane;
-
-    int _windowX = (_inputXPlane*100)/3;
-    int _windowY = (_inputYPlane*100)/3;
-
-    Sokoban Sokoban;
+    SokobanMap levelLayout;
     try {
-        //  Sokoban = Sokoban("nbody/starfield.jpg");
-        std::cin >> Sokoban;
+        std::ifstream fin (argv[1]);
+        fin >> levelLayout;
     }
-    catch (std::invalid_argument& err) {
-        std::cout << err.what() << std::endl;
+    catch(std::invalid_argument& err) {
+        std::cout << err.what()<<std::endl;
         exit(-1);
     }
 
+    int _windowX = (levelLayout.getColumn()*100)/3;
+    int _windowY = (levelLayout.getRow()*100)/3;
+
+    // window size should be getsize of one image then * by perimeter size
     sf::RenderWindow window(sf::VideoMode\
     (_windowX, _windowY), "Sokoban");
     window.setFramerateLimit(60);
 
-    // get Sokoban data from file input
-    // std::cin >> Sokoban;
+
+
 
 // SFML display loop
     while (window.isOpen()) {
@@ -41,6 +37,7 @@ int main(int argc, char* argv[]) {
             if (event.type == sf::Event::Closed) window.close();
         }
         window.clear();
+        window.draw(levelLayout);
         window.display();
     }
 }
